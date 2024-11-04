b62f49e2fb16cc6ec0feb3257a62bcc66da33060
Luz Rodriguez <lerodr20@asu.edu>
2024-11-03 20:50:13 -0700

    added rock paper scissors game against computer

--- rps.py
+++ rps.py
@@ -1,2 +1,22 @@
 import random
 
+def play():
+    user = input("Input 'r' for rock, 'p' for paper, 's' for scissors: ")
+    computer = random.choice(['r', 'p', 's'])
+    print(f'Computer chose: {computer}')
+
+    if user == computer:
+        return 'It\'s a tie!'
+    
+    if iswin(user, computer):
+        return 'You won!'
+    
+    return 'You lost!'  # No need to check iswin == False here, since it's implied
+
+def iswin(player, opponent):
+    # Returns True if the player beats the opponent
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+# Call play and print its result
+print(play())
